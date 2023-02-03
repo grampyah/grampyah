3095b89e77a601f91f9c65dc709edd09bf0a1b63
grampyah <118790128+grampyah@users.noreply.github.com>
2023-02-03 18:33:32 +0800

    Add files via upload

---
+++ ForShanny.js
@@ -0,0 +1,52 @@
+#cf80ff<DOCTYPE html>
+<html>
+<head>
+   <title> It's me Kimmy </title>
+   <script type="text/javascript">
+   alert("Hoy! Shanny")
+   alert("I hope you're doing well")
+   alert("here's a little something I made you!")
+   alert("hope you'll like it!")
+   </script>
+   <style>
+body{
+   background-image:url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVV6794vpcLXCwtsFoPUmMOVjKYRZ2MiMLHA&usqp=CAU);
+   background-size: cover;
+   background-attachment: fixed;
+}
+.content{
+    background: https://c.tenor.com/i1rsgMyOFgcAAAAd/cat-cat-love.gif;
+    width: 50%;
+    padding: 40px;
+    margin: 100px auto;
+    }
+</style>
+   <img src="https://media.tenor.com/gCfCRufPNysAAAAi/jinzhan-lily-and-marigold.gif"/>
+  <h2> <p style="color: black"> I'm sorry for my late replies, I'm kinda bit busy</p></h2>
+   
+   <img src="https://media.tenor.com/tbAhMwJ5bWEAAAAi/jinzhan-lily-and-marigold.gif"/>
+   <h2><p style="color: black"> I hope you're doing fine</p></h2>
+   
+   <img src="https://media.tenor.com/M8T3s8xVt9gAAAAi/reitciencias-messages.gif">
+   <h2><p style="color: black""> If you're having a hard time, and if you need someone to talk to,I'm one message away.</p></h2>
+   
+   <img src="https://media.tenor.com/HwU72GfcTwwAAAAi/youtube-superchat.gif">
+   <h2><p style="color: black""> goodluck in your studies, and don't forget to always believe in yourself!</p></h2>
+   
+   <img src="https://media.tenor.com/_DvuXLCVUWEAAAAi/cat-gray.gif">
+   <h2><p style="color: black""> I know you have the ability to do any kind of work weather it is easy or tough because you're smart</p></h2>
+   
+   <img src="https://media.tenor.com/NEzJbfb9GycAAAAi/molly-jinzhan.gif">
+   <h2><p style="color: black""> and also be confident of who you are.</p></h2>
+   
+   <img src="https://media.tenor.com/8_cC-JBZU2YAAAAi/meiling-hong.gif">
+   <h2><p style ="color: black""> whatever it is I'm proud of you!</p></h2>
+   
+   <img src="https://media.tenor.com/Ff6MHwCSC1oAAAAj/molly-jinzhan.gif">
+   <h2><p style ="color: black""> I know most of what I'm saying doesn't make sense but, you get the point.</p></h2>
+   
+   <img src="https://media.tenor.com/yN0WfVWCZ74AAAAi/tontonfriends-chubby.gif">
+   <h1 style="font-family:Times New Roman">FIGHTING!</h1>
+   
+   </body>
+   </html>
